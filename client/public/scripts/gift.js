06438968cf937a55c71e7b440f21b1e1fd165142
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-09-14 16:29:11 +0900

    Created detail page with style

---
+++ client/public/scripts/gift.js
@@ -0,0 +1,32 @@
+const renderGift = async () => {
+  const requestedID = parseInt(window.location.href.split("/").pop())
+
+  const res = await fetch(`/gifts`)
+  const data = await res.json()
+
+  const giftContent = document.getElementById("gift-content")
+  let gift = data.find((gift) => gift.id === requestedID)
+
+  if (gift) {
+    const nameElem = document.getElementById("name")
+    const submittedByElem = document.getElementById("submittedBy")
+    const pricePointElem = document.getElementById("pricePoint")
+    const audienceElem = document.getElementById("audience")
+    const descriptionElem = document.getElementById("description")
+    const imageElem = document.getElementById("image")
+
+    document.title = gift.name
+    imageElem.src = gift.image
+    nameElem.textContent = `${gift.name}`
+    submittedByElem.textContent = `Submitted by: ${gift.submittedBy}`
+    pricePointElem.textContent = `Price: ${gift.pricePoint}`
+    audienceElem.textContent = `Great For: ${gift.audience}`
+    descriptionElem.textContent = `${gift.description}`
+  } else {
+    const noGiftMsg = document.createElement("h2")
+    noGiftMsg.textContent = "No Gifts Available 😞"
+    giftContent.appendChild(noGiftMsg)
+  }
+}
+
+renderGift()
--- client/public/scripts/header.js
+++ client/public/scripts/header.js
@@ -6,7 +6,7 @@ headerContainer.className = "header-container"
 
 // left header div
 const logo = document.createElement("img")
-logo.src = "unearthed-logo.png"
+logo.src = "../unearthed-logo.png"
 logo.alt = "UnEarthed Logo"
 
 const title = document.createElement("h1")
